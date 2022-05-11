5a3ae36cc2fa833403d00a51099254e04e51c6be
Cristina <cristinaponce.designer@gmail.com>
2022-05-11 22:29:20 +0200

    feat(): added routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { HomeComponent } from './views/home/home.component';
+import { MoviesDetailComponent } from './views/movies-detail/movies-detail.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: ':id', component: MoviesDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
