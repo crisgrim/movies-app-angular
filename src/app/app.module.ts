b27e6e44228e7fd9b3fc35aa9f870be33106f030
Cristina <cristinaponce.designer@gmail.com>
2022-05-11 22:28:20 +0200

    feat(): added components and dependencies in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { MoviesItemComponent } from './components/movies-item/movies-item.component';
+import { MoviesDetailComponent } from './views/movies-detail/movies-detail.component';
+import { HomeComponent } from './views/home/home.component';
+import { SearchModuleComponent } from './components/search-module/search-module.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    MoviesItemComponent,
+    MoviesDetailComponent,
+    HomeComponent,
+    SearchModuleComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
